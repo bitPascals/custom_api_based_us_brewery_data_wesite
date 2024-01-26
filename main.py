cf2df4e7de05604076af447437090e4a617026e4
bitPascals <pascaluzosike@gmail.com>
2024-01-26 16:38:11 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+import requests
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+parameters = {
+    "page": 12,
+    "per_page": 4
+}
+
+response = requests.get("https://api.openbrewerydb.org/v1/breweries/")
+results = response.json()
+
+
+@app.route("/")
+def home():
+    all_results = results
+    return render_template("index.html", breweries=results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
